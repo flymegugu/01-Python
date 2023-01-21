e06b6d520b383a1fb48d13c8288ab12ea806befd
root <root@SGHJCG5059.cn.YumChina.com>
2023-01-21 12:13:32 +0800

    add

--- PYTHON3 100例/t.py
+++ PYTHON3 100例/t.py
@@ -2,7 +2,6 @@
 Author: [MaxGu]
 Date: 2022-12-18 22:10:17
 LastEditors: [MaxGu]
-LastEditTime: 2022-12-18 22:10:20
+LastEditTime: 2023-01-21 12:04:24
 Description:
 '''
-def 
--- PYTHON3 100例/test.py
+++ PYTHON3 100例/test.py
@@ -2,7 +2,7 @@
 Author: [MaxGu]
 Date: 2022-08-24 22:31:54
 LastEditors: [MaxGu]
-LastEditTime: 2023-01-13 11:04:56
+LastEditTime: 2023-01-20 12:49:34
 Description:
 '''
 
@@ -17,5 +17,3 @@ def pattern(n):
             print("*", end=" ")
         print("\r")
 
-
-pattern(5)
