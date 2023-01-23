0d3083c82e7275abbffb0099509d4d4f348435dd
maxgu <flyme007@yeah.net>
2023-01-23 20:50:21 +0800

    add

--- 1.1-基础巩固/20-函数/self.py
+++ 1.1-基础巩固/20-函数/self.py
@@ -1,10 +1,17 @@
+'''
+Author: [MaxGu]
+Date: 2022-08-07 18:09:45
+LastEditors: [MaxGu]
+LastEditTime: 2023-01-23 20:49:16
+Description:
+'''
 class Dog:
     def __init__(self,name,age,sex="女孩"):
         self.name = name
         self.age = age
-        self.sex = sex 
+        self.sex = sex
     def run(self):
-        print("{0}is  running".format(self.name))
+        print("{0}岁的狗狗 {1} is running".format(self.age,self.name))
     def speak(self,sound):
         print("{0} is speak {1}".format(self.name,sound))
 
